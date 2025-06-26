b9a9ce5905810c528bbe810b21905030f421b11f
Ricardo Campos <ricardo@DESKTOP-1JVBK1Q.localdomain>
2025-06-26 11:01:04 -0300

    feat: add Item and TodoList models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_131822) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_140008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.integer "status"
+    t.bigint "todo_list_id", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_todo_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +43,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_131822) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "items", "todo_lists"
+  add_foreign_key "todo_lists", "users"
 end
